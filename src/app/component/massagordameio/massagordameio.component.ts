0c5a278c663295270b1562c3f0c3379de1f77f75
armytoia <vitoriaacaldeirinha@gmail.com>
2019-05-02 01:31:25 -0300

    terceira_versao

--- src/app/component/massagordameio/massagordameio.component.ts
+++ src/app/component/massagordameio/massagordameio.component.ts
@@ -6,17 +6,27 @@ templateUrl : 'massagordameio.component.html'
 })
 
 export class MassaGordaMeioComponent{
-nome : string;
-idade: number = 0;
+  nome : string;
+  idade: number = 0;
+  telefone: number = 0;
+  email: string;
+  senha: string;
+  confsenha: string;
 
-sim(): void{
-alert("Parabéns! Rumo a uma vida melhor");
-}
-nao():void{
-alert("Não perca mais tempo e comece enquanto há tempo!");
-}
-cadastrar(): void{
-let idade = Number(this.idade);
-alert("Cadastro realizado com sucesso!");
-}
+
+  sim(): void{
+    alert("Parabéns! Rumo a uma vida melhor");
+  }
+  nao():void{
+    alert("Não perca mais tempo e comece enquanto há tempo!");
+  }
+  cadastrar(): void{
+    let idade = Number(this.idade);
+    let telefone = Number(this.telefone);
+    if(senha =! confsenha){
+      alert("Confira sua senha novamente")  
+    }else{
+      alert("Cadastro realizado com sucesso!");
+    }
+  }
 }
